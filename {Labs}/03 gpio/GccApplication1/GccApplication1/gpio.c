6e9ee38a7152863c4f6c1d1ca7413f72ec60f8d3
student <student@PC-629.urel.feec.vutbr.cz>
2021-10-05 14:51:00 +0200

    Lab2

---
+++ {Labs}/03 gpio/GccApplication1/GccApplication1/gpio.c
@@ -0,0 +1,72 @@
+
+
+/* Includes ----------------------------------------------------------*/
+#include "gpio.h"  //NEEDED FOR prototypes
+#include <avr/sfr_defs.h>
+/* Function definitions ----------------------------------------------*/
+/**********************************************************************
+ * Function: GPIO_config_output()
+ * Purpose:  Configure one output pin in Data Direction Register.
+ * Input:    reg_name - Address of Data Direction Register, such as &DDRB
+ *           pin_num - Pin designation in the interval 0 to 7
+ * Returns:  none
+ **********************************************************************/
+void GPIO_config_output(volatile uint8_t *reg_name, uint8_t pin_num);
+{
+    *reg_name = *reg_name | (1<<pin_num);
+}
+
+/**********************************************************************
+ * Function: GPIO_config_input_nopull()
+ **********************************************************************/
+
+/**********************************************************************
+ * Function: GPIO_config_input_pullup()
+ * Purpose:  Configure one input pin and enable pull-up.
+ * Input:    reg_name - Address of Data Direction Register, such as &DDRB
+ *           pin_num - Pin designation in the interval 0 to 7
+ * Returns:  none
+ **********************************************************************/
+void GPIO_config_input_nopull(volatile uint8_t *reg_name, uint8_t pin_num);
+{
+    *reg_name = *reg_name & ~(1<<pin_num);  //Clear Data Direction Register
+    reg_name++;                     // Change pointer to Data Register
+    *reg_name = *reg_name & ~ (1<<pin_num);   //Clear  Data Register
+}
+
+/**********************************************************************
+ * Function: GPIO_write_low()
+ * Purpose:  Write one pin to a low value.
+ * Input:    reg_name - Address of Port Register, such as &PORTB
+ *           pin_num - Pin designation in the interval 0 to 7
+ * Returns:  none
+ **********************************************************************/
+void GPIO_write_low(volatile uint8_t *reg_name, uint8_t pin_num);
+{
+    *reg_name = *reg_name & ~(1<<pin_num);
+}
+
+/**********************************************************************
+ * Function: GPIO_write_high()
+ **********************************************************************/
+void GPIO_write_high(volatile uint8_t *reg_name, uint8_t pin_num);
+{
+    *reg_name = *reg_name  | (1<<pin_num);
+}
+/**********************************************************************
+ * Function: GPIO_toggle()
+ **********************************************************************/
+void GPIO_toggle(volatile uint8_t *reg_name, uint8_t pin_num);
+{
+    *reg_name = *reg_name  ^ (1<<pin_num);
+}
+/**********************************************************************
+ * Function: GPIO_read()
+ **********************************************************************/
+uint8_t GPIO_read(volatile uint8_t *reg_name, uint8_t pin_num);
+{
+    if(bit_is_set(*reg_name,pin_num))
+           return 1;
+    else
+          return 0;
+}
